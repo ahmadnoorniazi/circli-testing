55a2e5a51382cb02a3fd31aa696bc88ee6d8e62f
ahmadnoorniazi <bc180201966@vu.edu.pk>
2018-12-05 19:35:22 +0500

    33

--- server/modules/acl/index.js
+++
@@ -1,5 +0,0 @@
-const { sequelize } = require('../../models')
-const { Acl, SequelizeStore } = require('@aclify/aclify')
-const acl = new Acl(new SequelizeStore(sequelize, { prefix: 'acl_' }));
-
-module.exports =  acl
